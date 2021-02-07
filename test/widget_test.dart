5843061a278ed57a21a2acef1dcc163ddbe1aae1
Harsha Manoj Suraweera <harshamanoj912@gmail.com>
2021-02-07 18:45:14 +0530

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:am_sliit/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LoadWelcomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
